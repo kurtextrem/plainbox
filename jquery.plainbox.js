f24b93b7f4a43c47c3b3b87f1b4e304d6350d1e3
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2015-10-03 19:27:51 +0200

    Add support for backward / forward

--- jquery.plainbox.js
+++ jquery.plainbox.js
@@ -4,6 +4,7 @@
 	var	document = window.document,
 		name = 'simplebox',
 		cls = '.' + name,
+		inClass = 'in',
 		_$a = $('<a class="' + name + '" style="" data-href=""></a>')
 		_$a.css({
 			display: 'block',
@@ -23,41 +24,55 @@
 			e.preventDefault()
 
 			var 	url = e.currentTarget.href || e.relatedTarget.src,
-				elem = document.querySelector(cls + '[data-href="' + url + '"]')
+				elem = document.querySelector(cls + '[data-href="' + url + '"]'),
+				full = e.currentTarget.dataset.link || ''
 
+			history.pushState({ pictureURL: url, fullLink:  full }, 'Image ' + url, full)
 			if (elem !== null) {
+				elem.classList.add(inClass)
 				elem.style.display = 'block'
 				return
 			}
 
-			if (url) {
-				var 	$a = _$a.clone(),
-					style = {
-						'background-image': 'url(' + url + ')'
-					},
-					img = new Image()
+			if (!url) return
 
-				this.append($a)
+			var 	$a = _$a.clone(),
+				style = {
+					'background-image': 'url(' + url + ')'
+				},
+				img = new Image()
 
-				img.onload = function (e) {
-					var width = e.target.width,
-						height = e.target.height
+			this.append($a)
 
-					if (width > window.innerWidth || height > window.innerHeight)
-						style['background-size'] = 'contain'
+			img.onload = function (e) {
+				var width = e.target.width,
+					height = e.target.height
 
-					$a.attr({
-						href: url,
-						'data-href': url
-					})
-					$a.css(style)
-				}
-				img.src = url
+				if (width > window.innerWidth || height > window.innerHeight)
+					style['background-size'] = 'contain'
+
+				$a.attr({
+					href: url,
+					'data-href': url
+				})
+				$a.addClass(inClass)
+				$a.css(style)
 			}
+			img.onerror = function (e) {
+				$a.hide()
+			}
+			img.src = url
 		}.bind(this))
 		.on('click' + cls, cls, function (e) {
 			e.preventDefault()
 			e.currentTarget.style.display = 'none'
+			e.currentTarget.classList.remove(inClass)
+		})
+
+		window.addEventListener('popstate', function (state) {
+			$(cls + '.' + inClass).hide()
+			if (state.fullLink && document.location !== state.fullLink)
+				document.querySelector(cls + '[data-link="' + state.fullLink + '"]').click()
 		})
 
 		return this
