a4455c8355c6754bf642c9077cee653eb90c9280
Jacob Groß <kurtextrem@gmail.com>
2017-05-19 09:33:30 +0200

    Add comments, resize listener
    and switch replaceState with history.back

--- jquery.plainbox.js
+++ jquery.plainbox.js
@@ -12,18 +12,28 @@
 	'use strict'
 
 	var document = window.document,
-		history = window.history,
-		_clientWidth = null,
-		_clientHeight = null
+		history = window.history
 
 	// var VERSION = '1.0'
 
+	var _clientWidth = null
+	/**
+	 * Caches and returns `window.innerWidth` (as it causes layout/reflow)
+	 *
+	 * @returns {Number}
+	 */
 	function clientWidth() {
 		if (_clientWidth === null)
 			_clientWidth = window.innerWidth
 		return _clientWidth
 	}
 
+	var _clientHeight = null
+	/**
+	 * Caches and returns `window.innerHeight` (as it causes layout/reflow)
+	 *
+	 * @returns {Number}
+	 */
 	function clientHeight() {
 		if (_clientHeight === null)
 			_clientHeight = window.innerHeight
@@ -31,114 +41,87 @@
 	}
 
 	/**
-	 * @const
-	 */
-	var SETTINGS = {
-		id: 'plainbox',
-		className: 'plainbox',
-
-		/**
-		 * Parent of the Plainbox.
-		 * @type {string}
-		 * @kind jQuery Selector
-		 */
-		parent: '',
-
-		loadingURL: 'https://s4db.net/assets/img/goalpost.gif',
-
-		errorURL: 'https://s4db.net/errors/assets/img/pet_crying.png',
-		error: 'Error',
-		errorTimeout: 5000,
-
-		/**
-		* Selector of the Plainbox.
-		* @type {string}
-		* @kind jQuery Selector
-		*/
-		_selector: '',
-		_loading: '',
-		_error: '',
-		_parent: null
-	}
-
-	/**
 	 * Holds the Plainbox.
 	 * @type {jQuery}
 	 * @kind jQuery Object
 	 */
-	var NODE = null,
-		nodeIsVisible = false
+	var NODE = null
+
+	/**
+	 * Whether or not the Plainbox is currently visible.
+	 * @type {Boolean}
+	 */
+	var nodeIsVisible = false
+
+	/**
+	 * Whether or not the Plainbox node has been appended to the DOM.
+	 */
+	var nodeInDom = false
+
+	/**
+	 * CSS props that are added to the Plainbox on show.
+	 * @const
+	 */
+	var nodeStyle = {
+		'background-image': '',
+		'background-size': ''
+	}
+
+	/**
+	 * Creates a new anchor node and styles it.
+	 *
+	 * @param {String} _loading background Image CSS prop
+	 * @returns {jQuery}
+	 */
 	function getNode(_loading) {
 		return $(document.createElement('a'))
 			.css({
+				// Easier centering
 				display: 'flex',
+				// Position
 				position: 'fixed',
 				top: '0',
 				left: '0',
 				right: '0',
 				bottom: '0',
+				// Image backkground
 				background: 'rgba(0, 0, 0, 0.85) center center no-repeat',
+				// Plainbox image
 				'background-image': _loading,
+				// Performance
 				'z-index': '99999',
 				contain: 'strict',
+				// Animation
 				opacity: '0',
 				'will-change': 'opacity',
 				transition: 'opacity 300ms ease-in-out',
-				// Text
+				// Error Text
 				'justify-content': 'center',
 				'align-items': 'center',
 				'text-decoration': 'none'
 			})
 	}
 
-	var _timeout = null // timeout
-	var style = {
-		'background-image': '',
-		'background-size': ''
-	}
-	var originalURL = location.href
+	/**
+	 * Returns a valid value for the CSS prop background-image.
+	 *
+	 * @param {String} url
+	 * @returns {String}
+	 */
+	function getUrlValue(url) { return 'url("' + url + '")' }
 
-	function getUrlValue(string) { return 'url("' + string + '")' }
+	/**
+	 * Holds the current timeout
+	 * @type {Number}
+	 * @kind window.setTimeout returned value
+	 */
+	var _timeout = null
 
-	var _state = { plainbox: true, plainboxUrl: '', plainboxImg: '' }
-	function clickEvent(e) {
-		var url = e.currentTarget.href || e.currentTarget.src
-
-		if (!url) return true
-
-		e.preventDefault()
-
-		var img = e.currentTarget.dataset.image || url // either take data-image or href / src
-		show(this, img, url)
-
-		originalURL = location.href
-		_state.plainboxUrl = url
-		_state.plainboxImg = img
-		history.pushState(_state, '', url)
-
-		return false
-	}
-
-	function _show(e) {
-		if (e !== undefined && (e.target.width > clientWidth() || e.target.height > clientHeight()))
-			style['background-size'] = 'contain'
-
-		NODE.css(style)
-	}
-
-	function loadImg(imgURL, url) {
-		var elem = NODE[0],
-			img = new Image()
-
-		elem.href = url
-		style['background-image'] = getUrlValue(imgURL)
-
-		img.onload = _show
-
-		img.src = imgURL
-		return img
-	}
-
+	/**
+	 * Hides the Plainbox.
+	 *
+	 * @param {SETTINGS} settings
+	 */
 	function hide(settings) {
 		var el = NODE[0]
 		window.clearTimeout(_timeout)
@@ -154,42 +137,21 @@
 			el.style.display = 'none'
 			el.style.backgroundImage = settings._loading
 			el.style.backgroundSize = ''
-			style['background-size'] = ''
+			nodeStyle['background-size'] = ''
 			el.textContent = ''
 		})
 	}
 
-	var _stateFalse = { plainbox: false }
-	function show(settings, imgURL, url) {
-		var img = loadImg(imgURL, url)
-		img.onerror = function onerror() {
-			style['background-image'] = settings._error
-			NODE[0].textContent = settings.error
-
-			_show()
-
-			if (settings.errorTimeout) {
-				window.clearTimeout(_timeout)
-				_timeout = window.setTimeout(function timeout() {
-					hide(settings)
-					history.replaceState(_stateFalse, '', originalURL)
-				}, settings.errorTimeout)
-			}
-		}
-
-		img = null // free for GC
-
-		showNode(settings)
-	}
-
-	/** Whether the Node has been appended or not. */
-	var _inDom = false
-	/** Shows/appends the node to the DOM. */
+	/**
+	 * Fades in the Plainbox and sets the correct class / ID.
+	 *
+	 * @param {SETTINGS} settings
+	 */
 	function showNode(settings) {
 		var elem = NODE[0]
 
-		if (!_inDom) {
-			_inDom = true
+		if (!nodeInDom) {
+			nodeInDom = true
 			settings._parent.append(elem)
 		}
 
@@ -205,14 +167,113 @@
 		})
 	}
 
+	/**
+	 * Sets the correct Plainbox image.
+	 *
+	 * @param {Event} [e]
+	 */
+	function showImg(e) {
+		if (e !== undefined && (e.target.width > clientWidth() || e.target.height > clientHeight()))
+			nodeStyle['background-size'] = 'contain'
+
+		NODE.css(nodeStyle)
+	}
+
+	/**
+	 * Creates a new Image object and loads it.
+	 *
+	 * @param {String} imgURL
+	 * @param {String} url
+	 * @returns {Image}
+	 */
+	function loadImg(imgURL, url) {
+		var elem = NODE[0],
+			img = new Image()
+
+		elem.href = url
+		nodeStyle['background-image'] = getUrlValue(imgURL)
+
+		img.onload = showImg
+
+		img.src = imgURL
+		return img
+	}
+
+	/**
+	 * Initiates loading and showing of the Plainbox image.
+	 *
+	 * @param {SETTINGS} settings
+	 * @param {String} imgURL
+	 * @param {String} url
+	 */
+	function show(settings, imgURL, url) {
+		var img = loadImg(imgURL, url)
+
+		img.onerror = function onerror() {
+			nodeStyle['background-image'] = settings._error
+			NODE[0].textContent = settings.error
+
+			showImg()
+
+			if (settings.errorTimeout) {
+				window.clearTimeout(_timeout)
+				_timeout = window.setTimeout(function timeout() {
+					hide(settings)
+					history.back()
+				}, settings.errorTimeout)
+			}
+		}
+
+		img = null // free for GC
+
+		showNode(settings)
+	}
+
+	var _state = { plainbox: true, plainboxUrl: '', plainboxImg: '' }
+	/**
+	 * Callback to clicks on thumbs.
+	 *
+	 * @this {SETTINGS}
+	 * @param {Event} e jQuery Event
+	 * @returns {Boolean}
+	 */
+	function clickEvent(e) {
+		var url = e.currentTarget.href || e.currentTarget.src
+
+		if (!url) return true
+
+		e.preventDefault()
+
+		var img = e.currentTarget.dataset.image || url // either take data-image or href / src
+		show(this, img, url)
+
+		_state.plainboxUrl = url
+		_state.plainboxImg = img
+		history.pushState(_state, '', url)
+
+		return false
+	}
+
+	/**
+	 * Callback to Plainbox click / keyup that closes it.
+	 *
+	 * @this {SETTINGS}
+	 * @param {Event} e jQuery Event
+	 */
 	function closeEvent(e) {
 		e.preventDefault()
 		if (e.type === 'click' || (e.type === 'keyup' && e.keyCode === 27)) {
 			hide(this)
-			history.replaceState(_stateFalse, '', originalURL)
+			history.back()
 		}
 	}
 
+	/**
+	 * Callback to global popstate.
+	 *
+	 * @this {SETTINGS}
+	 * @param {Event} e jQuery Event
+	 */
 	function onPopState(e) {
 		var state = e.originalEvent.state
 		if (state !== null && state.plainbox === true) {
@@ -223,29 +284,103 @@
 	}
 
 	/**
-	 * @param 	{string} selector jQuery selector for elements which trigger the Plainbox
-	 * @param 	{object} options
-	 * @returns {jQuery} 					Chainable
+	 * Reevaluate client width / height when the browser has been resized.
+	 */
+	function onResize() {
+		_clientWidth = null
+		_clientHeight = null
+	}
+
+	/**
+	 * Debounce `callback` (trailing call)
+	 *
+	 * @param {Function} callback
+	 * @param {Number} timeout Throttle amount in MS
+	 * @param {Number} [_time]
+	 * @returns {Function}
+	 */
+	function debounce(callback, timeout, _time) {
+		timeout = timeout || 100
+
+		return function debounce() {
+			window.clearTimeout(_time)
+			_time = window.setTimeout(callback, timeout)
+		}
+	}
+
+	/**
+	 * @typedef {Object} SETTINGS
+	 * @property {String} id
+	 * @property {String} className
+	 * @property {String} parent 			Parent of the Plainbox (jQuery Selector)
+	 * @property {String} loadingURL
+	 * @property {String} errorURL
+	 * @property {String} error
+	 * @property {Number} errorTimeout Timeout in MS after which to close the error
+	 *
+	 * @property {String} _selector 	Selector of the Plainbox (jQuery Selector)
+	 * @property {String} _loading 		CSS value for the loadingURL
+	 * @property {String} _error 			CSS value for the errorURL
+	 * @property {jQuery} _parent 		Parent jQuery object
+	 */
+	/**
+	 * Default settings object.
+	 * @namespace
+	 */
+	var SETTINGS = {
+		id: 'plainbox',
+		className: 'plainbox',
+
+		parent: '',
+
+		loadingURL: 'https://s4db.net/assets/img/goalpost.gif',
+
+		errorURL: 'https://s4db.net/errors/assets/img/pet_crying.png',
+		error: 'Error',
+		errorTimeout: 5000,
+
+		/**
+		* Selector of the Plainbox.
+		* @type {String}
+		* @kind jQuery Selector
+		* @private
+		*/
+		_selector: '',
+		_loading: '',
+		_error: '',
+		_parent: null
+	}
+
+	/**
+	 * @this {jQuery} Container to which the click listener is added to
+	 * @param {String} selector jQuery selector for elements which trigger the Plainbox
+	 * @param {SETTINGS} options
+	 * @returns {jQuery} Container to which the click listener is added to
 	 */
 	$.fn.plainbox = function plainbox(selector, options) {
+		/** @type {SETTINGS} */
 		var settings = $.extend(SETTINGS, options || {})
 
 		settings._loading = getUrlValue(settings.loadingURL) // loading animation
 
-		if (!_inDom) // only create node once
+		if (!nodeInDom) // only create node once
 			NODE = getNode(settings._loading)
 
 		var _selector = settings._selector = '.' + settings.className
 		settings._error = getUrlValue(settings.errorURL)
 		settings._parent = $(settings.parent || document.body)
 
-		this.on('click' + _selector, selector, clickEvent.bind(settings)) // click on any thumb
-		settings._parent.on('click' + _selector + ' keyup' + _selector, _selector, closeEvent.bind(settings)) // click / ESC on plainbox image
-		$(window).off('popstate.plainbox').on('popstate.plainbox', onPopState.bind(settings)) // we only want one popstate listener at the same time
+		/** Click on any thumb */
+		this.on('click' + _selector, selector, clickEvent.bind(settings))
+		/** Plainbox click / "ESC" */
+		settings._parent.on('click' + _selector + ' keyup' + _selector, _selector, closeEvent.bind(settings))
+		/** Popstate listener (only one at a time) */
+		$(window).off('popstate.plainbox').on('popstate.plainbox', onPopState.bind(settings))
+		$(window).off('resize.plainbox').on('resize.plainbox', debounce(onResize, 100))
 
 		var state = history.state
 		if (state === null) { // new state, add closed state
-			history.replaceState(_stateFalse, '', location.href)
+			history.replaceState({ plainbox: false }, '', location.href)
 		} else if (state.plainbox === true) { // we recover state after browser restart etc
 			show(settings, state.plainboxImg, state.plainboxUrl)
 		} else { // add to current state
